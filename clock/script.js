438e5812b53c20bfe70962e93c64e9501d50b3d9
debasish414 <162704473+debasish414@users.noreply.github.com>
2024-04-07 22:20:36 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,41 @@
+
+setInterval(() => {
+    let date = new Date();
+    let gethoure = date.getHours()
+    let  getminute = date.getMinutes()
+    let getsec = date.getSeconds()
+    let getday = date.getDay()
+    let getmonth = date.getMonth()
+    let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let prepand = (gethoure >= 12) ? " PM " : " AM ";
+    gethoure = (gethoure >= 12) ? gethoure - 12 : gethoure;
+    if (gethoure === 0 && prepand === ' PM ') {
+        if (getminute === 0 && getsec === 0) {
+            gethoure = 12;
+            prepand = ' Noon';
+        } else {
+            gethoure = 12;
+            prepand = ' PM';
+        }
+    }
+
+    if (gethoure === 0 && prepand === ' AM ') {
+        if (getminute === 0 && getsec === 0) {
+            gethoure = 12;
+            prepand = ' Midnight';
+        } else {
+            gethoure = 12;
+            prepand = ' AM';
+        }
+    }
+
+    document.getElementById("houre").innerText = gethoure + '-';
+    document.getElementById("minute").innerText = getminute + '-';
+    document.getElementById("second").innerText = getsec;
+    document.getElementById("zone").innerText = `- ${prepand}`;
+    document.getElementById("date").innerText = date.getDate() + '-';
+    document.getElementById("month").innerText = month[getmonth] + '-';
+    document.getElementById("year").innerText = date.getFullYear();
+    document.getElementById("day").innerText = day[getday]
+}, 1000);
